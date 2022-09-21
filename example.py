d2f9f501f9a52d76811432b5437e06e62a5abab3
Paulo Sandoval <ez2torta@gmail.com>
2022-09-20 23:56:11 -0300

    added requirements.txt

--- example.py
+++ example.py
@@ -3,7 +3,6 @@ import time
 import os
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
-from threading import Thread
 import pyaudio
 import wave
 
@@ -12,24 +11,28 @@ from typing import Any
 
 p = pyaudio.PyAudio()
 
+
 def play_sample(sample_file_name):
     # you audio here
-    wf = wave.open(sample_file_name, 'rb')
+    wf = wave.open(sample_file_name, "rb")
     # define callback
     def callback(in_data, frame_count, time_info, status):
         data = wf.readframes(frame_count)
         return (data, pyaudio.paContinue)
 
     # open stream using callback
-    return p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                channels=wf.getnchannels(),
-                rate=wf.getframerate(),
-                output=True,
-                stream_callback=callback)
+    return p.open(
+        format=p.get_format_from_width(wf.getsampwidth()),
+        channels=wf.getnchannels(),
+        rate=wf.getframerate(),
+        output=True,
+        stream_callback=callback,
+    )
+
 
 class MyHandler(PatternMatchingEventHandler):
     audio_playing: bool = False
-    stream : Any = None
+    stream: Any = None
     cached_file: str = ""
     cached_timestamp: Any = 0
 
@@ -41,11 +44,10 @@ class MyHandler(PatternMatchingEventHandler):
         sample = self.cached_file.split(" ")[0]
         print(sample)
         if sample == "ichigeki":
-            self.stream = play_sample('ichigeki.wav')
+            self.stream = play_sample("ichigeki.wav")
         else:
-            self.stream = play_sample('ha-ha.wav')
+            self.stream = play_sample("ha-ha.wav")
         self.stream.start_stream()
-
 
     def _check_cached_file(self, event):
         path = event.src_path
@@ -74,11 +76,8 @@ class MyHandler(PatternMatchingEventHandler):
 
     def process(self, event):
         pass
-        # print("I am being processed")
 
     def on_modified(self, event):
-        # breakpoint()
-        # print("file modified " + event.src_path)
         if self._check_cached_file(event):
             self._play_audio_file()
         self.process(event)
@@ -88,7 +87,7 @@ if __name__ == "__main__":
     args = sys.argv[1:]
     observer = Observer()
     observer.schedule(MyHandler(), path=args[0] if args else ".")
-    print("Start")
+    print("Observer Working")
     observer.start()
 
     try:
